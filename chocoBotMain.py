afde1f22cc018ac07d893782bbb4eabb1107459b
jarvis09 <contact-jarvis09@laposte.net>
2022-10-01 21:56:49 +0200

    Update cmd choco: dico liens d'img déplacé dans chocodico.py

--- chocoBotMain.py
+++ chocoBotMain.py
@@ -7,6 +7,8 @@ import discord
 from discord.ext import commands
 import os
 import random
+import chocodico # dico de liens d'images pour la cmd choco
+from chocodico import dico
 
 intents = discord.Intents.all()  # intents, je sais trops a quoi sa sert mais sans mon code marche pas
 bot = commands.Bot(command_prefix="$", intents=intents)  # le préfixe et leur truc barbare
@@ -58,38 +60,9 @@ $say [texte] renvoie que vous avez écrit après la commande
 
 @bot.command()
 async def choco(ctx):
-
     choco_choice = random.randrange(1,12)
-    chocodico = {
-"1": "https://www.lafilleduboulanger.ca/wp-content/uploads/fdb-chocolatine.jpg",
-"2": "https://www.lafilleduboulanger.ca/wp-content/uploads/fdb-chocolatine.jpg",
-"3": "https://www.chocoblast.fr/wp-content/uploads/%C3%8Ates-vous-plut%C3%B4t-chocolatine-ou-pain-au-chocolat.jpg",
-"4": "https://www.chocoblast.fr/wp-content/uploads/%C3%8Ates-vous-plut%C3%B4t-chocolatine-ou-pain-au-chocolat.jpg",
-"5": "http://chokoalafraise.c.h.pic.centerblog.net/e5d59abf.png",
-"6": "https://www.francetvinfo.fr/image/75it0saxe-08f3/1500/843/15108688.jpg",
-"7": "https://www.telegraph.co.uk/content/dam/news/2018/05/24/TELEMMGLPICT000164508497_trans%2B%2BkZdGXfzOPVSbO-9sH583RFqaPefEGY05nXXPGqlPqGQ.jpeg",
-"8": "https://www.parisvox.info/wp-content/uploads/2018/05/painauchocolat-950x400.jpg",
-"10": "https://cache.cosmopolitan.fr/data/photo/w1000_ci/1bj/pain-au-chocolat-chocolatine.jpg",
-"11": "https://www.lepoint.fr/images/2018/05/23/14952239lpw-14952489-article-des-pains-au-chocolat-aussi-appeles-chocolatine-jpg_5266450_540x282.jpg",
-"12": "",
-"13": "",
-"15": "",
-"16": "",
-"17": "",
-"18": "",
-"19": "",
-"20": "",
-"21": "",
-"22": "",
-"23": "",
-"24": "",
-"25": "",
-"26": "",
-"27": "",
-"28": "",
-}
-    rdm_choco = random.choice(list(chocodico))
-    await ctx.send(chocodico[f"{rdm_choco}"])
+    rdm_choco = random.choice(list(dico))
+    await ctx.send(dico[f"{rdm_choco}"])
     log(f"{ctx.author.name} a executé la commande choco, choco choisie:{rdm_choco} ")
     print(f"{ctx.author.name} a executé la commande choco, choco choisie: {rdm_choco} ")
 
