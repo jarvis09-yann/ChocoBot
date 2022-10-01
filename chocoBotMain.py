c08d1aed12db8dd92dbe72bf9b3aecd51d0cf7d3
jarvis09 <contact-jarvis09@laposte.net>
2022-10-01 18:27:51 +0200

    V1.1: EMBEDS pour toutes les cmds + nettoyage & fixs + NEW TODO

--- chocoBotMain.py
+++ chocoBotMain.py
@@ -17,6 +17,12 @@ def log(msg):
         heure = time.ctime()  # Un truc utile pour mettre des heures dans les logs !
         log_it.write(f"{heure} | {msg}\n")
 
+cant_use_cmd=discord.Embed(title="Attention, vous n'avez pas accès a cette commande", color=0xedf305)
+cant_use_cmd.set_author(name="Vous ne pouvez pas utiliser cette commande !",url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+cant_use_cmd.set_thumbnail(url="https://media.tenor.com/4qOJaZloJj4AAAAj/tag.gif")
+cant_use_cmd.set_footer(text="Team Chocolatine")
+
+
 @bot.event
 async def on_ready():
     print("Le Choco bot est en ligne")
@@ -28,27 +34,38 @@ async def on_ready():
 async def help(ctx):
     log(f"commande help enclenchée par {ctx.author.name}")
     print(f"commande help enclenchée par {ctx.author.name}")  # les logs de la console
-    await ctx.send(""" 
-	**Aide relative aux commandes**:
-*Préfixe:* _**$**_\n
+    embed = discord.Embed(description="""
+        *Préfixe:* _**$**_\n
 **UTILITAIRE:**
- $info: affiche des informations relatives au serveur
- $about: donne des informations à propos du bot\n
+ $info: affiche le nombre de membres sur le serveur
+ $about: donne des informations à propos du bot
  $ping: permet de connaitre la latence du bot\n					
-**MODERATION:**
-*la plupart des commandes ci dessous sons à usage exclusif des administrateurs***       
- $ban [@utilisateur] [raison]: banni l'utilisateur mentionné
- $unban [@utilisateur] [raison]: débanni l'utilisateur mentionné
+**MODÉRATION:**
+***NOTE: les commandes de la section MODÉRATION sonts uniquement utilisables par les Administrateurs***       
+ $ban [@utilisateur] [raison]: ban l'utilisateur mentionné
+ $unban [@utilisateur] [raison]: déban l'utilisateur mentionné
  $kick [@utilisateur] [raison]: expulse l'utilisateur mentionné\n
  **FUN:**
 *ces commandes peuvent être utlisées par tout les utilisateurs*
-$say [texte] renvoie se que vous avez écrit	""")  # le texte de la cmd
+$say [texte] renvoie que vous avez écrit après la commande
+    """, color=0x008000)
+    embed.set_author(name="AIDE RELATIVE AUX COMMANDES", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    embed.set_thumbnail(url="https://media.tenor.com/GXMu0NRMHQgAAAAC/question-mark.gif")
+    embed.set_footer(text="Team Chocolatine")
+    await ctx.send(embed=embed)
+
 
 
 @bot.command()
 async def about(ctx):  # commande about
     log(f"commande about éxécutée par {ctx.author.name}")
-    await ctx.send(f"Le Choco bot est un bot discord dédié au serveur Team Chocolatine (https://discord.gg/wZ5aNWk33y) il a été développé par jarvis09#1787 & FiFolker#9350")
+    embed = discord.Embed(
+        title="Le Choco bot est un bot discord dédié au serveur Team Chocolatine (https://discord.gg/wZ5aNWk33y) il a été développé par jarvis09#1787 & FiFolker#9350",
+        color=0xff0000)
+    embed.set_author(name="A propos du chocobot", url="https://discord.gg/wZ5aNWk33y")
+    embed.set_thumbnail(url="https://media.tenor.com/b7HjoHE1K4QAAAAj/i%CC%87nfo-icon.gif")
+    embed.set_footer(text="Team Chocolatine")
+    await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -66,7 +83,11 @@ async def rick(ctx):  # ma commande (qui rickroll, marche que pour mon ID)
 
 @bot.command()  # la commande ping, celle qui utilise le module time
 async def ping(ctx):
-    await ctx.send(f"pong !\n latence de {bot.latency} secondes")
+    embed = discord.Embed(title=f"latence de {bot.latency}ms !", color=0x340cf3)
+    embed.set_author(name="Pong !", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    embed.set_thumbnail(url="https://media.tenor.com/c9WptHOa_LMAAAAM/pong.gif")
+    embed.set_footer(text="Team Chocolatine")
+    await ctx.send(embed=embed)
     print(f"commande ping enclanchée par {ctx.author.name} !")
     log(f"commande ping enclanchée par {ctx.author.name}, la latene du bot est de {bot.latency}ms")
 
@@ -79,7 +100,7 @@ async def info(ctx):  # quelques infos a propos du serveur
     msg = f"Le serveur **{ServName}** a **{Nofofmembers}** membres !"
     await ctx.send(msg)
     print(f"commande info enclanchée par {ctx.author.name} !")
-    log("commande info executée par {ctx.author.name}")
+    log(f"commande info executée par {ctx.author.name}")
 
 
 @bot.command()  # un truc pr le fun comme ça
@@ -94,11 +115,18 @@ async def kick(ctx, user: discord.User, *reason):
     if ctx.message.author.guild_permissions.administrator:
         reason = " ".join(reason)
         await ctx.guild.kick(user, reason=reason)
-        await ctx.send(f"**{user} à été expulsé pour la raison :*{reason}* **")
+        embed = discord.Embed(title=f"{user} a été kick pour la raison {reason}, que cella lui serve de leçon !", color=0xedf305)
+        embed.set_author(name="KICK !", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        icon_url="https://cdn.discordapp.com/emojis/961270811644289094.webp?size=128&quality=lossless")
+        embed.set_thumbnail(url="https://media.tenor.com/sWGfjLcAEDwAAAAC/kick-cartoon.gif")
+        embed.set_footer(text="Team Chocolatine")
+        await ctx.send(embed=embed)
+        ctx.send(embed=embed)
+
         print(f"{ctx.author.name} a expulsé {user} pour la raison {reason}")
         log(f"{ctx.author.name} a expulsé {user} pour la raison {reason}")
     else:
-        await ctx.send("**Erreur, vous n'avez pas la permission de faire ça !**")
+        await ctx.send(embed=cant_use_cmd)
         print(f"{ctx.author.name} a tenté d'utiliser la commande kick sur {user} pour la raison {reason} !")
         log(f"""{ctx.author.name} a tenté d'utiliser la commande kick sur {user} pour la raison {reason},
             mais il n'avait pas la permission amdinistrateur donc la commande a échouée.""")
@@ -109,11 +137,16 @@ async def ban(ctx, user: discord.User, *reason):
     reason = " ".join(reason)
     if ctx.message.author.guild_permissions.administrator:
         await ctx.guild.ban(user, reason=reason)
-        await ctx.send(f"**{user}** à été banni pour la raison suivante : **{reason}**")
+        ban_embed = discord.Embed(title=f"{user} a été banni pour la raison: {reason} que cella lui serve de leçon !", color=0xff0000)
+        ban_embed.set_author(name="BAN !", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                             icon_url="https://cdn.discordapp.com/emojis/961270811644289094.webp?size=128&quality=lossless")
+        ban_embed.set_thumbnail(url="https://media.tenor.com/Kt1irdU_daUAAAAS/ban-admin.gif")
+        ban_embed.set_footer(text="Team Chocolatine")
+        await ctx.send(embed=ban_embed)
         print(f"{ctx.author.name} a utilisé la commande ban sur {user} pour la raison {reason} !")
-        log(f"{ctx.author.name} a tenté d'utiliser la commande kick sur {user} pour la raison {reason} !")
+        log(f"{ctx.author.name} a tenté d'utiliser la commande ban sur {user} pour la raison {reason} !")
     else:
-        await ctx.send("**Vous n'avez pas la permission de faire ceci !**")
+        await ctx.send(embed=cant_use_cmd)
         log(f"""{ctx.author.name} a tenté d'utiliser la commande ban sur {user} pour la raison {reason},
         mais il n'avait pas la permission amdinistrateur donc la commande a échouée.""")
         print(f"""{ctx.author.name} a tenté d'utiliser la commande ban sur {user} pour la raison {reason},
@@ -129,16 +162,21 @@ async def unban(ctx, user, *reason):
         for i in bannedUsers:
             if i.user.name == nom and i.user.discriminator == userID:
                 await ctx.guild.unban(i.user.discriminator, reason=reason)
-                await ctx.send(f"{user} à été débanni !")
+                embed = discord.Embed(title=f"{member} a été unban par {ctx.author.name} pour la raison: {reason} !",
+                                      color=0x1111e8)
+                embed.set_author(name="UNBAN", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+                embed.set_thumbnail(url="https://media.tenor.com/_4K_0sndwtEAAAAi/green-white.gif")
+                embed.set_footer(text="Team Chocolatine")
+                await ctx.send(embed=embed)
                 print(f"{ctx.author.name} a utilisé la commande deban ban sur {user} pour la raison {reason} !")
-                log(f"{ctx.author.name} a utilisé la commande deban ban sur {user} pour la raison {reason}")
+                log(f"{ctx.author.name} a utilisé la commande deban sur {user} pour la raison {reason}")
                 return
         await ctx.send(f"L'utilisateur {user} n'est pas dans la banlist !")
         print(
             f"{ctx.author.name} a utilisé la commande deban sur {user} pour la raison {reason} mais il n'était pas dans la banlist !")
         log(f"{ctx.author.name} a tenté d'utilisé la commande deban sur {user} pour la raison {reason} mais il n'était pas dans la banlist")
     else:
-        await ctx.send("**Vous n'avez pas la permission de utiliser ça !**")
+        await ctx.send(embed=cant_use_cmd)
         print(f"""{ctx.author.name} a tenté d'utiliser la commande unban sur {user} pour la raison {reason}
             mais il n'avait pas la permission amdinistrateur donc la commande a échouée.""")
 
@@ -155,13 +193,17 @@ async def mute(ctx, member: discord.Member, *, reason=None):
             for channel in guild.channels:
                 await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
         await member.add_roles(mutedRole, reason=reason)
-        await ctx.send(f"{ctx.author.name} a rendu muet **{member.mention}* pour la raison **{reason}**")
+        embed = discord.Embed(title=f"{member} a été mute pour la raison: {reason},     que cela lui serve de leçon !", color=0x1111e8)
+        embed.set_author(name="MUTE !", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        embed.set_thumbnail(url="https://media.tenor.com/FWh2E4AQyTEAAAAM/mute.gif")
+        embed.set_footer(text="Team Chocolatine")
+        await ctx.send(embed=embed)
         log(f"{ctx.author.name} a rendu muet {member}* pour la raison **{reason}")
         print(f"**{ctx.author.name} a rendu muet {member.mention} pour la raison:** *{reason}*")
         await member.send(
             f"Vous avez été rendu muet sur le serveur **{guild.name}** pour la raison: **{reason}**")
     else:
-        await ctx.send("**Vous ne pouvez pas utiliser cette commande !**")
+        await ctx.send(embed=cant_use_cmd)
         log(f"""{ctx.author.name} a tenté de rendre muet {membre} pour la raison {reason},
             mais il n'avait pas la permission amdinistrateur donc la commande a échouée.""")
 
@@ -172,10 +214,14 @@ async def unmute(ctx, member: discord.Member):
         mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
 
         await member.remove_roles(mutedRole)
-        await ctx.send(f"{member.mention} a été unmute")
+        embed = discord.Embed(title=f"{member} a été unmute par {ctx.author.name} !", color=0x1111e8)
+        embed.set_author(name="UNMUTE", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        embed.set_thumbnail(url="https://media.tenor.com/rZRx_DyeF8sAAAAi/symbols.gif")
+        embed.set_footer(text="Team Chocolatine")
+        await ctx.send(embed=embed)
         await member.send(f"Vous avez été unmute du serveur **{ctx.guild.name}** par **{ctx.author.name}**")
     else:
-        await ctx.send("**Vous n'avez pas la permission de faire ça !**")
+        await ctx.send(embed=cant_use_cmd)
         log(f"""{ctx.author.name} a tenté de unmute {member},
         mais il n'avait pas la permission amdinistrateur donc la commande a échouée.""")
 
